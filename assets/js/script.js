a33eb9cf4e39a4aacd793b6f004347f45c05009c
Muhammad Idrees <idrees_android@outlook.com>
2021-09-27 02:25:23 +0500

    add lots of features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+new WOW().init();
+$(".portfolio-item").isotope({
+    itemSelector :  '.item',
+    layoutMode : 'fitRows'
+});
+$(".portfolio-menu ul li").click(function(){
+    $(".portfolio-menu ul li").removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+    $('.portfolio-item').isotope({
+        filter : selector
+    });
+});
 function progressBarScroll() {
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
       height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
